ed2270d37a93aeaaa3de650a01fc7884a939dccd
Zubin Juárez <zubinxxi@gmail.com>
2025-09-18 21:06:45 -0500

    se actualizó el modelo para que acepte cantidad en varchar

--- dashboard_tutorial/model/user_model.py
+++ dashboard_tutorial/model/user_model.py
@@ -1,9 +1,10 @@
 from typing import Optional
 import reflex as rx
-from sqlmodel import Field 
+from sqlmodel import Field, Column
+from sqlalchemy.dialects import mysql
 
 class Users(rx.Model, table=True):
-    login: str
-    user_name: str
-    email: str
-    gender: str
+    login: str = Field(sa_column=Column(mysql.VARCHAR(255)))
+    user_name: str = Field(sa_column=Column(mysql.VARCHAR(255)))
+    email: str = Field(sa_column=Column(mysql.VARCHAR(255)))
+    gender: str = Field(sa_column=Column(mysql.VARCHAR(255)))
